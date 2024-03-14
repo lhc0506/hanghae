a8f548ff49e7e80c4165b58d0340bfc0af1fc0d8
Chan <lhc0506@gmail.com>
2024-03-14 10:03:46 +0900

    Chore: Add babel eslint and edit rc

--- packages/chapter1/.eslintrc.cjs
+++ packages/chapter1/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: {node: true, browser: true, es2020: true, jest: true},
   extends: ['eslint:recommended', 'eslint-config-prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@babel-eslint',
   settings: {
     'import/resolver': {
       node: {
