d2494e7476fab8fe97598c17eca0479f8f74fe28
Chan <lhc0506@gmail.com>
2024-03-20 22:36:22 +0900

    Chore: Set eslint and prettier

---
+++ packages/chapter2/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {node: true, browser: true, es2020: true, jest: true},
+  extends: ['eslint:recommended', 'eslint-config-prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@babel-eslint',
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
