6264eb654abc1e8b7cfeb9777cb84db6bad1a239
Chan <lhc0506@gmail.com>
2024-03-25 13:28:13 +0900

    Chore: Set eslint

--- packages/chapter1/.eslintrc.cjs
+++ packages/chapter1/.eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: {node: true, browser: true, es2020: true, jest: true},
   extends: ['eslint:recommended', 'eslint-config-prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@babel-eslint',
+  parser: '@babel/eslint-parser',
   settings: {
     'import/resolver': {
       node: {
--- packages/chapter2/.eslintrc.cjs
+++ packages/chapter2/.eslintrc.cjs
@@ -3,7 +3,14 @@ module.exports = {
   env: {node: true, browser: true, es2020: true, jest: true},
   extends: ['eslint:recommended', 'eslint-config-prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@babel-eslint',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   settings: {
     'import/resolver': {
       node: {
