10ea11e3b4c6c81f36d42f7df6165935c9364368
Chan <lhc0506@gmail.com>
2024-04-03 22:30:23 +0900

    Feat: Add Helmet

--- packages/chapter3/src/index.js
+++ packages/chapter3/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Helmet, HelmetProvider} from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+      <Helmet>
+        <title>Hello World!!!!</title>
+        <meta name="description" content={'this is description'} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://example.com/page.html" />
+        <meta property="og:title" content="Content Title" />
+        <meta
+          property="og:image"
+          content="https://hanghae99.spartacodingclub.kr/_next/image?…%2Fplus_fe_course_image.png%3Fver%3D2&w=3840&q=75"
+        />
+        <meta property="og:description" content="Description Here" />
+        <meta property="og:site_name" content="Site Name" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630"></meta>
+      </Helmet>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
